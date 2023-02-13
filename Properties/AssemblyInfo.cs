672a6f47af2578c3ca16b25f7c697667ab314b72
Farros <86004023+farroshsy@users.noreply.github.com>
2023-02-13 20:02:56 +0700

    feat: todolist_app

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TODOFormApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TODOFormApp")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0eb411b3-7fa4-4135-a8d2-174531837945")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
